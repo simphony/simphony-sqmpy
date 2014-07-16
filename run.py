b75af9c43632a709897923c6ce75fdda0d8f3660
Mehdi Sadeghi <sade@iwm.fraunhofer.de>
2014-07-16 11:51:46 +0200

    Clean up in run.py and others. New user in db.

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 from sqmpy import app
 
 if __name__ == '__main__':
-    #import logging
-    #logging.basicConfig(filename='sqmpy.log', level=logging.DEBUG)
-    #app = init_app()
-
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
--- sqmpy/job/forms.py
+++ sqmpy/job/forms.py
@@ -4,19 +4,28 @@
 
     Implements job management forms.
 """
-from flask.ext.wtf import RecaptchaField, Form
-from wtforms import StringField, BooleanField, TextAreaField, FileField, SelectField, validators
-from wtforms.validators import Required, EqualTo, Email, Optional
+from flask.ext.wtf import Form
+#from flask.ext.wtf.html5 import URLField
+from wtforms import StringField, TextAreaField, FileField, SelectField, validators
 
 __author__ = 'Mehdi Sadeghi'
+
+
+class InputFileForm(Form):
+    """
+    To get list of input files
+    """
+
 
 
 class JobSubmissionForm(Form):
     """
     Form to handle job submission.
     """
-    name = StringField('Name', [Required(), validators.Length(min=1, max=50)])
-    script = TextAreaField('Script', [Required()])
-    input_file = FileField('Input file', [Optional()])
-    resource = SelectField('Resource', [Required()], coerce=int)
-    description = TextAreaField('Description', [Optional()])
+    name = StringField('Name', [validators.Required(), validators.Length(min=1, max=50)])
+    script = TextAreaField('Script', [validators.Required()])
+    input_file = FileField('Input file', [validators.Optional()])
+    # choices will be filled at runtime
+    resource = SelectField('Resource', [validators.Required()], coerce=int)
+    description = TextAreaField('Description', [validators.Optional()])
+    #url = URLField(validators=[validators.url()])

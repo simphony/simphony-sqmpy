3be36a189b7248f4d59db243195131e977f41f91
Mehdi Sadeghi <sade@iwm.fraunhofer.de>
2015-04-16 12:58:30 +0200

    rq removed

--- sqmpy/__init__.py
+++ sqmpy/__init__.py
@@ -7,11 +7,7 @@
     to remote resources.
     `sqm' stands for Simple Queue Manager.
 """
-from rq import Queue
-from rq.job import Job
 from worker import conn
 
 __author__ = 'Mehdi Sadeghi'
 __version__ = 'v0.2'
-
-q = Queue(connection=conn)

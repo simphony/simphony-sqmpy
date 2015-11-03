fd498e076b82444a035f2ea93a9be3f3883ee180
Mehdi Sadeghi <mehdi.sadeghi@iwm.fraunhofer.de>
2015-11-03 15:33:19 +0100

    Update tests

--- tests.py
+++ tests.py
@@ -16,11 +16,13 @@ from sqmpy.factory import create_app
 class SqmpyLoginTestCase(unittest.TestCase):
     def setUp(self):
         self.db_fd, self.db_file = tempfile.mkstemp()
-        opts = {'SQLALCHEMY_DATABASE_URI': 'sqlite:///' + self.db_file + '?check_same_thread=False',
+        opts = {'SQLALCHEMY_DATABASE_URI': 'sqlite:///'
+                + self.db_file + '?check_same_thread=False',
                 'TESTING': True,
                 'WTF_CSRF_ENABLED': False,
                 'CSRF_ENABLED': False,
-                'LOGIN_DISABLED': False}
+                'LOGIN_DISABLED': False,
+                'USE_LDAP_LOGIN': False}
         app = create_app(**opts)
         self.client = app.test_client()
 
@@ -30,48 +32,52 @@ class SqmpyLoginTestCase(unittest.TestCase):
 
     def test_empty_db(self):
         rv = self.client.get('/')
-        assert 'You should be redirected automatically to target URL' in rv.data
+        assert 'You should be redirected automatically to target URL'\
+            in rv.data
 
     def login(self, username, password):
-        return self.client.post('/security/login', data=dict(
+        return self.client.post('/login', data=dict(
             username=username,
             password=password
         ), follow_redirects=True)
 
     def logout(self):
-        return self.client.get('/security/logout', follow_redirects=True)
+        return self.client.get('/logout', follow_redirects=True)
 
     def test_register(self):
-        rv = self.client.get('/security/register')
+        rv = self.client.get('/register')
         for word in ['Register', 'Username', 'Email', 'Password']:
             assert word in rv.data
-        rv = self.client.post('/security/register', data=dict(username='admin',
-                                                              password='default',
-                                                              confirm='default',
-                                                              email='abc@abc.com'),
+        rv = self.client.post('/register',
+                              data=dict(username='admin',
+                                        password='default',
+                                        confirm='default',
+                                        email='abc@abc.com'),
                               follow_redirects=True)
         # We should have been forwarded to login page
-        assert '<li class="active"><a href="/">Home</a></li>' in rv.data
+        assert 'registered' in rv.data
         # Logout
         self.logout()
         # Posting the same info should raise an error since the user exists
-        rv = self.client.post('/security/register', data=dict(username='admin',
-                                                              password='default',
-                                                              confirm='default',
-                                                              email='abc@abc.com'),
+        rv = self.client.post('/register',
+                              data=dict(username='admin',
+                                        password='default',
+                                        confirm='default',
+                                        email='abc@abc.com'),
                               follow_redirects=True)
-        assert 'User with similar information already exists' in rv.data
+        assert 'already exists' in rv.data
 
     def test_login_logout(self):
-        rv = self.client.post('/security/register', data=dict(username='admin',
-                                                              password='default',
-                                                              confirm='default',
-                                                              email='abc@abc.com'),
+        rv = self.client.post('/register',
+                              data=dict(username='admin',
+                                        password='default',
+                                        confirm='default',
+                                        email='abc@abc.com'),
                               follow_redirects=True)
         # We first logout, because register logged us in
         rv = self.logout()
         # We will be forwarded to login page with a warning message
-        assert 'To visit / on this server you need to sign in.' in rv.data
+        assert 'log in to access this page' in rv.data
         rv = self.login('adminx', 'default')
         assert 'Invalid username' in rv.data
         rv = self.login('admin', 'default')
@@ -79,4 +85,3 @@ class SqmpyLoginTestCase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-

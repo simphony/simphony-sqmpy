ef418ae3fca3bba6d7c90c0ae1fdf0515506ee09
Mehdi Sadeghi <mehdi.sadeghi@iwm.fraunhofer.de>
2016-01-18 16:37:48 +0100

    Delete unused run script

--- tornado_server.py
+++
@@ -1,8 +0,0 @@
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-from sqmpy import create_app
-
-http_server = HTTPServer(WSGIContainer(create_app('../config.py')))
-http_server.listen(3000)
-IOLoop.instance().start()
